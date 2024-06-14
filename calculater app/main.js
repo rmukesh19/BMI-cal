ec824af6d1633782c4e8de4978cbe75ee65c97ad
R. Mukesh <133189677+rmukesh19@users.noreply.github.com>
2024-06-14 19:37:25 +0530

    Add files via upload

---
+++ calculater app/main.js
@@ -0,0 +1,23 @@
+const outputScreen = document.querySelector('input');
+
+function display(num) {
+    outputScreen.value += num;
+}
+
+function Calculate() {
+    try {
+        outputScreen.value = eval(outputScreen.value)
+    }
+    catch (err) {
+        alert("Invalid!")
+    }
+}
+function Reset() {
+    outputScreen.value = "";
+}
+function Delete() {
+    outputScreen.value = outputScreen.value.slice(0, -1)
+}
+
+
+
